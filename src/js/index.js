61d8c6570d78914b28867eeaf678103c9c8dd630
isamocellin <isabelarequiaomocellin@hotmail.com>
2024-04-07 14:38:28 -0300

    Botão mostrar Menos e atualizações de animação

--- src/js/index.js
+++ src/js/index.js
@@ -1,36 +1,77 @@
 /*
-    Objetivo 1 - Quando o usuário clicar no botão de mostrar mais deve abrir os projetos escondidos no html.
-        Passo 1 - Pegar o botão mostrar mais no JS pra poder verificar quando o usuário clicar em cima dele
-        Passo 2 - Indentificar o clique no botão
-        Passo 3 - Adicionar a classe "ativo" nos projetos escondidos
+    Objetivo 1 - quando o usuário clicar no botão de mostrar mais deve abrir os projetos que estão escondidos no html
 
-    Objetivo 2 - Esconder o botão de mostrar mais
-        Passo 1- Pegar o botão e esconder ele
+        Passo 1 - pegar o botão mostrar mais no JS pra poder verificar quando o usuário clicar em cima dele
+
+        Passo 2 - identificar o clique no botão
+        
+        Passo 3 - adicionar a classe "ativo" nos projetos escondidos
+
+    Objetivo 2 - esconder o botão de mostrar mais
+        Passo 1 - pegar o botão e esconder ele
 */
-//    Objetivo 1 - Quando o usuário clicar no botão de mostrar mais deve abrir os projetos escondidos no html.
-// Passo 1 - Pegar o botão mostrar mais no JS pra poder verificar quando o usuário clicar em cima dele
-const botaoMostrarProjetos = document.querySelector('.btn-mostrar-projetos');
-const projetosInativos = document.querySelectorAll('.projeto:not(.ativo');
 
-botaoMostrarProjetos.addEventListener("click", () => {
-    //Passo 3 - Adicionar a classe "ativo" nos projetos escondidos
+// Objetivo 1 - quando o usuário clicar no botão de mostrar mais deve abrir os projetos que estão escondidos no html
+
+// Passo 1 - pegar o botão mostrar mais no JS pra poder verificar quando o usuário clicar em cima dele
+const botaoMostrarProjetos = document.querySelector('.btn-mostrar-projetos');
+const botaoOcultarProjetos = document.querySelector('.btn-ocultar-projetos');
+const projetosInativos = document.querySelectorAll('.projeto:not(.ativo)');
+
+botaoMostrarProjetos.addEventListener('click', () => {
+    // Passo 3 - adicionar a classe "ativo" nos projetos escondidos
     mostrarMaisProjetos();
 
-    //  Objetivo 2 - Esconder o botão de mostrar mais
-    //  Passo 1- Pegar o botão e esconder ele
+    // Objetivo 2 - esconder o botão de mostrar mais
+    // Passo 1 - pegar o botão e esconder ele
     esconderBotao();
 });
 
+function situacaoBotao() {
+    const projetosVisiveis = document.querySelectorAll('.projeto.ativo');
+    
+    if (projetosVisiveis.length > 0) {
+        // Se há projetos visíveis, exibir o botão "Mostrar Menos" e ocultar o botão "Mostrar Mais"
+        botaoOcultarProjetos.style.display = 'inline-block';
+        botaoMostrarProjetos.style.display = 'none';
+    } else {
+        // Se não há projetos visíveis, exibir o botão "Mostrar Mais" e ocultar o botão "Mostrar Menos"
+        botaoOcultarProjetos.style.display = 'none';
+        botaoMostrarProjetos.style.display = 'inline-block';
+    }
+}
+
+
+
+
 function esconderBotao() {
     botaoMostrarProjetos.classList.add("remover");
+    botaoOcultarProjetos.classList.remove("remover");
 }
 
 function mostrarMaisProjetos() {
     projetosInativos.forEach(projetoInativo => {
-        projetoInativo.classList.add("ativo");
+        projetoInativo.classList.add('ativo');
     });
 }
 
+
+botaoOcultarProjetos.addEventListener('click', () => {
+    ocultarProjetos();
+
+    mostrarBotao();
+})
+
+function mostrarBotao() {
+    botaoMostrarProjetos.classList.remove("remover");
+    botaoOcultarProjetos.classList.add("remover");
+}
+
+function ocultarProjetos() {
+    projetosInativos.forEach(projetoInativo => {
+        projetoInativo.classList.remove('ativo');
+    });
+}
 //Animação do Texto:
 //Titúlo
 
@@ -118,7 +159,7 @@ document.addEventListener('DOMContentLoaded', function() {
         }
         
         // Adicionar atraso antes de começar a gerar o texto
-        setTimeout(adicionarCaractereComAtraso, 3700); // Atraso de 1000 milissegundos (1 segundo)
+        setTimeout(adicionarCaractereComAtraso, 4400); // Atraso de 1000 milissegundos (1 segundo)
     });
 });
 
